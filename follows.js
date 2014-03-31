4b54992eed847401023b979c7e781cf374a03685
jbare <jbare7@gmail.com>
2014-03-30 20:04:30 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+	var defaultKey		= '9dJLmHiTme5tisMaa9p9hq0mkYayN6A1S2bofMvmtU27BHsH', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1315520261], // Co9dJLmHiTme5tisMaa9p9hq0mkYayN6A1S2bofMvmtU27BHsHmma separated array of Xively Feed ID numbers
+		applicationName	= 'Skales Aquaponics', // Replaces Xively logo in the header
+		dataDuration	= '100', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10, // Default interval for data to be displayed (in seconds)
+		dataColor		= '000000', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
