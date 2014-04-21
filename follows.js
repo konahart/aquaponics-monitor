7421fd571ce5179d47091f480a09531f58b0c142
jbare <jbare7@gmail.com>
2014-04-20 18:23:42 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '9dJLmHiTme5tisMaa9p9hq0mkYayN6A1S2bofMvmtU27BHsH', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [1315520261], // Co9dJLmHiTme5tisMaa9p9hq0mkYayN6A1S2bofMvmtU27BHsHmma separated array of Xively Feed ID numbers
+	var defaultKey		= '5O9bF3kIcw1GplantsNuxdOgSRmtft3sj7rrpfnaOYBEMCV4', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [593895726], // Co9dJLmHiTme5tisMaa9p9hq0mkYayN6A1S2bofMvmtU27BHsHmma separated array of Xively Feed ID numbers
 		applicationName	= 'Skales Aquaponics', // Replaces Xively logo in the header
 		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10, // Default interval for data to be displayed (in seconds)
